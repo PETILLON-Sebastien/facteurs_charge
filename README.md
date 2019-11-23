da4eff3bea39bece022543b992114a6a9dce5b0a
Sebastien <petillon.sebastien@gmail.com>
2019-11-24 00:23:22 +0100

    Création environnement webpack / babel / react

---
+++ README.md
@@ -0,0 +1,3 @@
+# Facteurs de charge
+
+Ceci est le projet de démonstration des facteurs de charge du parc électrique français.
---
+++ fullstack_facteurs_charge/static/webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const config = {
+    entry:  __dirname + '/js/index.jsx',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      ]
+    }
+};
+module.exports = config;
