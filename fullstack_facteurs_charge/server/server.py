97d85d91aa32896cc918a0c1ca0e077415e7e8a0
Sebastien <petillon.sebastien@gmail.com>
2019-11-24 22:38:08 +0100

    Premières données - à affiner

--- fullstack_facteurs_charge/server/server.py
+++ fullstack_facteurs_charge/server/server.py
@@ -1,8 +1,34 @@
 from flask import Flask, render_template
+import requests
+import arrow
+import json
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+
+# Charger les capacites installées
+fichier_capacites = open('data/puissances_installees.json', 'r')
+capacites_installees = fichier_capacites.read()
+fichier_capacites.close()
+print(capacites_installees)
+
+# Variables globales
+API_RESEAUX_ENERGIE = "https://opendata.reseaux-energies.fr/api/records/1.0/search/"
 
 @app.route("/")
 def index():
+    r = requests.session()
+    date = arrow.now(tz='Europe/Paris')
+    formatte = date.format('YYYY-MM-DDTHH:mm')
+
+    params = {
+        'dataset': 'eco2mix-national-tr',
+        'q': 'date_heure <= {}'.format(formatte),
+        'timezone': 'Europe/Paris',
+        'rows': 1
+    }
+    reponse = r.get(API_RESEAUX_ENERGIE, params=params)
+    donnees = json.loads(reponse.content)
+    print(donnees)
+
     return render_template("index.html")
 
 @app.route("/hello")
