6275838f696b1b8c877acabfa3f0d172351cd1b6
primary <facteus@localhost.localdomain>
2020-01-23 18:23:47 +0100

    Version presque fonctionnelle - limite taille fichier (bundle.js)

--- fullstack_facteurs_charge/server/server.py
+++ fullstack_facteurs_charge/server/server.py
@@ -1,9 +1,12 @@
-# -*-coding:Latin-1 -*
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template
 import requests
 import arrow
 import json
 import time
+import traceback
+from django.shortcuts import render
+from wsgiref import util
 app = Flask(__name__, static_folder="static", template_folder="static")
 
 # Informations générales
@@ -45,7 +48,7 @@ def init_donnees():
         donnees[region_key]['evolution'] = []
     donnees['0']['capacites'] = {}
     donnees['0']['evolution'] = []
-    
+
 def appel_necessaire():
     global dernier_appel
     global periode_rafraichissement
@@ -60,7 +63,7 @@ def calculs_regionaux(donnees_regional):
     for record in reversed(donnees_regional['records']):
         ligne_donnee = record['fields']
         nouvelle_donnee = {
-            'date_heure': ligne_donnee['date_heure'], 
+            'date_heure': ligne_donnee['date_heure'],
             'libelle_region': ligne_donnee['libelle_region']
         }
         donnees_region = donnees[str(ligne_donnee['code_insee_region'])]
@@ -71,7 +74,7 @@ def calculs_regionaux(donnees_regional):
                 nouvelle_donnee[source_energie] = float(ligne_donnee[source_energie])
             if cle_tch in ligne_donnee:
                 nouvelle_donnee[cle_tch] = float(ligne_donnee[cle_tch])
-            
+
             source_energie_non_calculee = source_energie not in donnees_region['capacites']
             source_energie_calculable = source_energie in nouvelle_donnee and cle_tch in nouvelle_donnee and nouvelle_donnee[cle_tch] > 0
             if source_energie_non_calculee and source_energie_calculable:
@@ -80,9 +83,9 @@ def calculs_regionaux(donnees_regional):
         for autre_donnee in autres_donnees:
             if autre_donnee in ligne_donnee:
                 nouvelle_donnee[autre_donnee] = float(ligne_donnee[autre_donnee])
-        
+
         donnees_region['evolution'].append(nouvelle_donnee)
-    
+
     # Calcul du plus petit nombre de donnees pour une region
     nombre_resultats_min = nombre_donnees_par_heure * nombre_heures
     for key in donnees:
@@ -158,7 +161,7 @@ def calcul_meilleur_facteur():
                         meilleure_valeur = donnee_courante[cle]
                         meilleure_cle = cle[len(tch_prefix):]
             donnees[code_insee_region]['meilleur_facteur'].append(meilleure_cle)
-                    
+
 
 API_RESEAUX_ENERGIE = "https://opendata.reseaux-energies.fr/api/records/1.0/search/"
 
@@ -184,7 +187,7 @@ def mise_a_jour_donnees():
     }
     reponse_regional = r.get(API_RESEAUX_ENERGIE, params=params_regional)
     donnees_regional = json.loads(reponse_regional.content.decode("utf-8"))
-    
+
     # Utile pour du debug
     # sauver(donnees_regional)
 
@@ -192,17 +195,94 @@ def mise_a_jour_donnees():
     calculs_regionaux(donnees_regional)
     calculs_nationaux()
 
-@app.route("/")
-def index():
-
+def verification_donnees():
     if appel_necessaire():
         global dernier_appel
         dernier_appel = arrow.now()
         mise_a_jour_donnees()
         calcul_meilleur_facteur()
 
+
+@app.route("/")
+def index():
+
+    verification_donnees()
     global donnees
-    return render_template("index.html", donnees=donnees)
+    return render_template("facteurscharge.html", donnees=donnees)
 
 if __name__ == "__main__":
     app.run()
+
+
+def serve_html(environ, start_response):
+    global donnees
+    verification_donnees()
+
+    html = '<html>'
+    html += '<head>'
+    html += '   <meta charset="utf-8">'
+    html += '       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>'
+    html += '       <link rel="stylesheet" href="static/css/style.css"/>'
+    html += '       <title>Facteurs de charge</title>'
+    html += '   </head>'
+    html += '   <body>'
+    html += '       <div id="content"></div>'
+    html += '       <script type="text/javascript">var donnees = %(donnees)s;</script>'
+    html += '       <script src="static/dist/bundle.js" type="text/javascript"></script>'
+    html += '   </body>'
+    html += '</html>'
+
+    response_body = html % {'donnees': donnees}
+
+    status = '200 OK'
+    response_headers = [
+        ('Content-Type', 'text/html')
+    ]
+    start_response(status, response_headers)
+    return [response_body]
+
+def serve_bundle(environ, start_response):
+    status = '200 OK'
+    response_headers = [
+        ('Content-Type', 'application/javascript')
+    ]
+    start_response(status, response_headers)
+    bundle = ''
+    response = []
+    length = 0
+    with open('/home/facteus/www/static/dist/bundle.js', 'r', encoding='utf-8') as file:
+        bundle = file.read()
+        length = len(bundle)
+        l = 0
+        block = 32768
+        while (l + block) < length:
+            yield bundle[l:l+block-1]
+            #response.append(bundle[l:l+block-1])
+            l += block
+        yield bundle[l:length-1]
+        #response.append(bundle[l:length-1])
+        #return [bundle[0:65536]]
+        #return [bundle]
+
+def serve_css(environ, start_response):
+    response_body = ''
+    with open('/home/facteus/www/static/css/style.css', 'r', encoding='utf-8') as file:
+        response_body = file.read()
+        file.close()
+    status = '200 OK'
+    response_headers = [
+        ('Content-Type', 'text/css'),
+        ('Content-Length', str(len(response_body)))
+    ]
+    start_response(status, response_headers)
+    return [response_body]
+
+def application(environ, start_response):
+    MAX_CONTENT  = 1048576
+    MAX_CHUNK = 1048576
+    REQUEST_URI = util.request_uri(environ)
+    if (REQUEST_URI.endswith('static/css/style.css')):
+        return serve_css(environ, start_response)
+    if (REQUEST_URI.endswith('static/dist/bundle.js')):
+        return serve_bundle(environ, start_response)
+    return serve_html(environ, start_response)
