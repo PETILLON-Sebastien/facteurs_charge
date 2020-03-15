617a4ae57a77a9f82d8cde11bad38c0b8706244b
Sebastien <petillon.sebastien@gmail.com>
2020-03-15 19:16:02 +0100

    Utilisation d'express

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 var _ = require('lodash');
 var moment = require('moment');
 var compute = require('./compute.js');
+var express = require('express')
+var app = express()
 
 var dernier_appel = undefined;
 var donnees = undefined;
@@ -11,64 +13,50 @@ var nombre_donnees_par_heure = 4;
 var nombre_heures = 25;
 var periode_rafraichissement = 5;
 
-
 function serveData(res) {
     res.writeHead(200, {"Content-Type": "application/javascript"});
-    // if(compute.appel_necessaire(dernier_appel, periode_rafraichissement)) {
-    //     compute.recuperation_donnes_api(nombre_region, nombre_donnees_par_heure, nombre_heures, function (api_response) {
-    //         try {
-    //             var donnees_calculees = compute.construire_donnees(api_response, nombre_donnees_par_heure, nombre_heures, true);
-    //             donnees = donnees_calculees;
-    //             dernier_appel = moment();
-    //         } catch (e) {
-    //             console.error(e);
-    //         }
-    //         res.end("var donnees = " + JSON.stringify(donnees) + ";");
-    //     });
-    // } else {
-    //     res.end("var donnees = " + JSON.stringify(donnees) + ";");
-    // }
-    
-    fs.readFile("donnees/donnees_2020-03-15.json", function read(err, data) {
-        if (err) {
-            throw err;
-        }
-        res.end("var donnees = " + data + ";");
-    });
-}
-
-function serveStaticFile(url, res) {
-    fs.readFile('./static' + url, function (err, data) {
-        if (err) {
-          res.writeHead(404);
-          res.end(JSON.stringify(err));
-          return;
-        }
-        if (_.endsWith(url, '.css')) {
-            res.writeHead(200, {"Content-Type": "text/css"});
-        } else if (_.endsWith(url, '.html')) {
-            res.writeHead(200, {"Content-Type": "text/html"});
-        } else if (_.endsWith(url, '.js')) {
-            res.writeHead(200, {"Content-Type": "application/javascript"});
-        } else if (_.endsWith(url, '.ico')) {
-            res.writeHead(200, {"Content-Type": "image/x-icon"});
-        } else {
-            res.writeHead(200);
-        }
-        res.end(data);
-      });
-}
-
-var server = http.createServer(function(req, res) {
-    if (_.endsWith(req.url, 'data.js')) {
-        serveData(res);
-    } else if (_.includes(req.url, '.ico')) {
-        serveStaticFile('/images/favicon.ico', res);
-    } else if (_.isEqual(req.url, "/")) {
-        serveStaticFile('/facteurscharge.html', res);
+    if(compute.appel_necessaire(dernier_appel, periode_rafraichissement)) {
+        compute.recuperation_donnes_api(nombre_region, nombre_donnees_par_heure, nombre_heures, function (api_response) {
+            try {
+                var donnees_calculees = compute.construire_donnees(api_response, nombre_donnees_par_heure, nombre_heures, true);
+                donnees = donnees_calculees;
+                dernier_appel = moment();
+            } catch (e) {
+                console.error(e);
+            }
+            res.end("var donnees = " + JSON.stringify(donnees) + ";");
+        });
     } else {
-        serveStaticFile(req.url, res);
+        res.end("var donnees = " + JSON.stringify(donnees) + ";");
     }
+    
+    // fs.readFile("donnees/donnees_2020-03-08.json", function read(err, data) {
+    //     if (err) {
+    //         throw err;
+    //     }
+    //     res.end("var donnees = " + data + ";");
+    // });
+}
 
+app.use(express.static('static'));
+app.use(express.static('donnees'));
+
+app.get('/', function (req, res) {
+    fs.readFile('./static/facteurscharge.html', function (err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        } else {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+            return;
+        }
+    });
 });
-server.listen(8080);
+
+app.get('/data.js', function (req, res) {
+    serveData(res);
+});
+
+app.listen(8080, () => console.log(`Application lancée`));
