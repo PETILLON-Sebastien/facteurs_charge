80e574b17423cb931bdb21790a30d6ca081d6515
primary <facteus@localhost.localdomain>
2020-03-15 18:32:21 +0100

    Correction du chemin vers les donnees dans le cron

--- crons/recuperation_donnees_veille.js
+++ crons/recuperation_donnees_veille.js
@@ -7,11 +7,10 @@ var nombre_heures = 24 * 7;
 
 function recuperer_donnees() {
     var date_debut = moment().subtract(1, 'day').startOf('day').subtract(1, "minute");
-
     compute.recuperation_donnes_api_date(nombre_region, nombre_donnees_par_heure, date_debut, function (api_response, nom) {
         try {
             var donnees_calculees = compute.construire_donnees(api_response, nombre_donnees_par_heure, nombre_heures, false);
-            compute.sauver(donnees_calculees, "../donnees/" + nom);
+            compute.sauver(donnees_calculees, "/home/facteus/www/donnees/" + nom);
         } catch (e) {
             console.error(e);
         }
