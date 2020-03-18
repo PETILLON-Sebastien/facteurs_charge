c9553bdad32541f509daa59742d021e51106defb
Sebastien <petillon.sebastien@gmail.com>
2020-03-18 19:42:23 +0100

    Navigation regions

--- server.js
+++ server.js
@@ -14,34 +14,31 @@ var nombre_heures = 25;
 var periode_rafraichissement = 5;
 
 function serveData(res) {
-    res.writeHead(200, {"Content-Type": "application/javascript"});
-    if(compute.appel_necessaire(dernier_appel, periode_rafraichissement)) {
-        compute.recuperation_donnes_api(nombre_region, nombre_donnees_par_heure, nombre_heures, function (api_response) {
-            try {
-                var donnees_calculees = compute.construire_donnees(api_response, nombre_donnees_par_heure, nombre_heures, true);
-                donnees = donnees_calculees;
-                dernier_appel = moment();
-            } catch (e) {
-                console.error(e);
-            }
-            res.end(JSON.stringify(donnees));
-        });
-    } else {
-        res.end(JSON.stringify(donnees));
-    }
+    res.writeHead(200, {"Content-Type": "application/json"});
+    // if(compute.appel_necessaire(dernier_appel, periode_rafraichissement)) {
+    //     compute.recuperation_donnes_api(nombre_region, nombre_donnees_par_heure, nombre_heures, function (api_response) {
+    //         try {
+    //             var donnees_calculees = compute.construire_donnees(api_response, nombre_donnees_par_heure, nombre_heures, true);
+    //             donnees = donnees_calculees;
+    //             dernier_appel = moment();
+    //         } catch (e) {
+    //             console.error(e);
+    //         }
+    //         res.end(JSON.stringify(donnees));
+    //     });
+    // } else {
+    //     res.end(JSON.stringify(donnees));
+    // }
     
-    // fs.readFile("donnees/donnees_2020-03-08.json", function read(err, data) {
-    //     if (err) {
-    //         throw err;
-    //     }
-    //     res.end("var donnees = " + data + ";");
-    // });
+    fs.readFile("donnees/donnees_2020-03-08.json", function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        res.end(data);
+    });
 }
 
-app.use(express.static('static'));
-app.use(express.static('donnees'));
-
-app.get('/', function (req, res) {
+function serveFrontEnd(res) {
     fs.readFile('./static/facteurscharge.html', function (err, data) {
         if (err) {
             res.writeHead(404);
@@ -53,6 +50,16 @@ app.get('/', function (req, res) {
             return;
         }
     });
+}
+
+app.use(express.static("static"));
+app.use(express.static("donnees"));
+
+app.get('/', function (req, res) {
+    serveFrontEnd(res);
+});
+app.get('/region/[0-9]+', function (req, res) {
+    serveFrontEnd(res);
 });
 app.get('/now', (req, res) => {
     serveData(res);
---
+++ static/js/Board.jsx
@@ -0,0 +1,149 @@
+import React, { createRef } from 'react'
+import Map from "./Map";
+import github from '../images/github.png';
+import buttonOn from '../images/buttonon.png';
+import buttonOff from '../images/buttonoff.png';
+import Representations from "./representations"
+import GrapheCharge from "./graphe-charge";
+import GrapheProduction from "./graphe-production";
+import Slider from 'rc-slider';
+import APropos from './a-propos'
+import 'rc-slider/assets/index.css';
+import moment from "moment";
+import regionDescription from "./regions-descriptions";
+import _ from "lodash";
+
+var SERVER = "http://localhost:8080";
+// var SERVER = "http://facteurs-charge.fr";
+
+var that;
+
+export default class Board extends React.Component {
+
+  constructor(props) {
+    super(props);
+    that = this;
+    this.grapheCharge = createRef();
+    this.grapheProduction = createRef();
+    this.regionsDescriptions = regionDescription;
+  }
+
+  componentDidMount() {
+    // Recuperation de la zone
+    var path = window.location.pathname;
+    var match = path.match(".*/region/([0-9]+)");
+    var id_zone = 0;
+    if(match) {
+      id_zone = Number.parseInt(match[1]);
+    }
+    
+    fetch(SERVER + '/now')
+    .then(res => res.json())
+    .then((donnees) => {
+      this.setState({
+        donnees: donnees,
+        actionsVisibles: true,
+        id_zone_selectionnee: id_zone,
+        donnees_zone: donnees[0],
+        index_temps: donnees[0].evolution.length - 1
+      });
+    })
+    .catch(console.log)
+  }
+
+  onSliderChange(valeur) {
+    that.setState({
+      index_temps: valeur
+    });
+    that.grapheCharge.current.modifierTemps(valeur);
+    that.grapheProduction.current.modifierTemps(valeur);
+  }
+
+  handleClick(indice_zone) {
+    
+    if(indice_zone == 0) {
+      this.props.history.push("/");
+    } else {
+      this.props.history.push("/region/" + indice_zone);
+    }
+    this.setState({
+      id_zone_selectionnee: indice_zone,
+      donnees_zone: this.state.donnees[indice_zone]
+    });
+  }
+
+  changerVisibiliteActions() {
+    that.setState({
+      actionsVisibles: !that.state.actionsVisibles 
+    });
+  }
+
+  render() {
+    if(!this.state) {
+      return "";
+    }
+    let meilleurs_facteurs = {};
+    for(var cle_region in this.state.donnees) {
+      meilleurs_facteurs[cle_region] = this.state.donnees[cle_region]['meilleur_facteur'];
+    }
+    let marks = {};
+    for(var index in this.state.donnees[0].evolution) {
+      if(index % 16 === 0) {
+        var donnee = this.state.donnees[0].evolution[index];
+        marks[index] = (moment(donnee.date_heure)).format("HH:mm");
+      }
+    }
+    
+    let donnee_region_selectionnee = _.find(this.regionsDescriptions, {'id': this.state.id_zone_selectionnee});
+    let label_region = donnee_region_selectionnee.label;
+    let label_date_heure = moment(this.state.donnees_zone.evolution[this.state.index_temps].date_heure).format("DD/MM/YY HH:mm");
+
+    let actions = this.state.actionsVisibles ? 
+      (<div className="actions">
+        <Map handleClick={(i) => this.handleClick(i)} 
+              meilleurs_facteurs={meilleurs_facteurs} 
+              zone_selectionnee={this.state.id_zone_selectionnee} 
+              index_temps={this.state.index_temps}/>
+        <Slider className="slider-temps" value={this.state.index_temps} marks={marks}
+          min={0} max={this.state.donnees[0].evolution.length - 1} onChange={this.onSliderChange}/>
+      </div>)
+      : null;
+
+    let boutonActions = this.state.actionsVisibles ? 
+      <img src={buttonOff} className="buttonOff" onClick={this.changerVisibiliteActions} alt="Cacher les actions" title="Cacher les actions"/>
+      : <img src={buttonOn} className="buttonOn" onClick={this.changerVisibiliteActions} alt="Montrer les actions" title="Montrer les actions"/>;
+
+    let classNameAffichage = this.state.actionsVisibles ? "affichage" : "affichage seul";
+
+    return (
+      <div className="app">
+        <div className="bandeau">
+          {boutonActions}
+          <div className="titre">
+            <span className="label-region">{label_region}</span>
+            <span className="label-date-heure">{label_date_heure}</span>
+          </div>
+          <div className="liens">
+            <a href="https://github.com/PETILLON-Sebastien/facteurs_charge">
+              <img src={github} alt="Projet github" title="Projet github"/>
+            </a>
+            <a href="https://twitter.com/PetillonSebast1">
+              <img src="https://pbs.twimg.com/profile_images/1141374395623071744/qmY4xWra_400x400.jpg" alt="Profil twitter" title="Profil twitter"/>
+            </a>
+          </div>
+        </div>
+        {actions}
+        <div className={classNameAffichage}>
+          <Representations className="representations" donnees={this.state.donnees_zone} index_temps={this.state.index_temps} />
+        </div>
+        <div className={classNameAffichage}>
+          <GrapheCharge donnees={this.state.donnees_zone.evolution} index_temps={this.state.index_temps}
+            ref={this.grapheCharge} actionsVisibles={this.state.actionsVisibles} />
+          <GrapheProduction donnees={this.state.donnees_zone.evolution} index_temps={this.state.index_temps}
+            ref={this.grapheProduction} actionsVisibles={this.state.actionsVisibles} />
+        </div>
+        <APropos />
+      </div>
+    );
+  }
+}
