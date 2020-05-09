783a5dc8848b74c51c5447312159a6efd7cf1fe9
Sebastien <petillon.sebastien@gmail.com>
2020-05-09 19:01:10 +0200

    Useless change to check build

--- static/js/a-propos.jsx
+++ static/js/a-propos.jsx
@@ -10,7 +10,7 @@ class APropos extends React.Component {
         return (<div className="aPropos">
             <span>Cette application s'appuie sur les données fournies par <a href="https://opendata.reseaux-energies.fr">
                 https://opendata.reseaux-energies.fr
-            </a>. Merci à benichou.</span>
+            </a>. Merci à benichouchou.</span>
             <span>Inspirations :&nbsp; 
                 <a href="https://www.electricitymap.org">https://www.electricitymap.org</a> et&nbsp;
                 <a href="https://nuclear-monitor.fr/#/">https://nuclear-monitor.fr/#/</a>.
