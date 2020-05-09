c42a5e5a9bafef338c89d5b862e4c2f6e47efe3c
PETILLON-Sebastien <petillon.sebastien@gmail.com>
2020-05-09 16:43:32 +0000

    Deploying to release-candidate from  @ 305e2ceea4e7e704c5a5c254be7b65136301a624 🚀

--- static/js/a-propos.jsx
+++ static/js/a-propos.jsx
@@ -10,7 +10,7 @@ class APropos extends React.Component {
         return (<div className="aPropos">
             <span>Cette application s'appuie sur les données fournies par <a href="https://opendata.reseaux-energies.fr">
                 https://opendata.reseaux-energies.fr
-            </a>.</span>
+            </a>. Merci à benichou.</span>
             <span>Inspirations :&nbsp; 
                 <a href="https://www.electricitymap.org">https://www.electricitymap.org</a> et&nbsp;
                 <a href="https://nuclear-monitor.fr/#/">https://nuclear-monitor.fr/#/</a>.
