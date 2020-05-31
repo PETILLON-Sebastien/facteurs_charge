996b555cf33cdf45985cae3851efa40b9d08b24e
ttben <benjamin.benni06@gmail.com>
2020-05-31 23:59:11 +0200

    Enable zone coloring given its highestload

--- static/js/slides/map/components/Zone.jsx
+++ static/js/slides/map/components/Zone.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PowerSourceStyleMap from '../../../power-sources/components/PowerSourceStyleMap';
 
 class Zone extends React.Component { 
   constructor(props) {
@@ -13,11 +14,12 @@ class Zone extends React.Component {
     const zoneDescription = this.props.description;
     const color =  this.props.color;
 
+    const powerSourceStyleMap = new PowerSourceStyleMap(zoneDescription.highestLoad);
+
     return (<path
         d={zoneDescription.path}
         transform={zoneDescription.transform}
-        className={`region`}
-        style={{"fill":color}}
+        className={`region region-${powerSourceStyleMap.classes}`}
         onClick={() => hookCallback(zoneDescription.id)}
         strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="9" display="inline">
     >
