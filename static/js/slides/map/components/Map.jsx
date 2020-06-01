dcf0a2bfcce3856a5fa6216440dbe2a89e46fca6
ttben <benjamin.benni06@gmail.com>
2020-06-01 11:16:12 +0200

    Fix undefined highestload when building navbar
    The navbar contains a Map, via  the embedded modal.
    It needed to have the highestLoad information for each zone ; a call that was made inside SlideMap...
    
    It duplicated the call.

--- static/js/slides/map/components/Map.jsx
+++ static/js/slides/map/components/Map.jsx
@@ -6,40 +6,58 @@ class Map extends React.Component {
   constructor(props) {
     super(props);
     // this.regionsDescriptions = regionDescription;
-    
+    this.state = {
+      data: this.get()
+    }
     this.zoneChanged = this.zoneChanged.bind(this);
   }
 
   zoneChanged(valeur) {
     this.props.zoneChanged(valeur);
   }
-  
-  getRandomColor() {
-    var letters = '56789ABCDEF';
-    var color = '#';
-    const c = Math.floor(Math.random() * 11);
-    for (var i = 0; i < 6; i++) {
-      color += letters[c];
-    }
-    return color;
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
   }
+
+  get() {
+    const installationsArray = ["solar", "wind", "nuclear", "hydraulic", "fossil", "bioenergies"];
+    let data = [];
+    for (let i = 0; i < 14; i++) {
+      data.push({
+        "id": i,
+        "highestLoad": {
+          "installation": installationsArray[this.getRandomInt(installationsArray.length - 1)],
+          "value": this.getRandomInt(101)
+        }
+      })
+    }
+
+    return data;
+  }
+
 
   render() {
     const regionsDescriptions = this.props.zonesDescription;
+
+    // fixme
+    // link zone with highestload (fake data)
+    regionsDescriptions.forEach((e, i) => {
+      e.highestLoad = this.state.data[i].highestLoad.installation
+    });
 
     let regions = [];
     for (let i = 0; i < regionsDescriptions.length; i++) {
       regions.push(
         <Zone
-          key={regionsDescriptions[i].id} 
+          key={regionsDescriptions[i].id}
           description={regionsDescriptions[i]}
-          color={this.getRandomColor()}
           zoneChanged={this.zoneChanged}
           onClick={(i) => this.zoneChanged(i)}
         />
       );
     }
-    
+
     return (
       <div className="map">
         <svg xmlns="http://www.w3.org/2000/svg" width="100%" display="inline" version="1" viewBox="0 0 650 520">
