9bf54c415f9c5b653e22cb5cede0f8c1143283f8
Sebastien <petillon.sebastien@gmail.com>
2020-06-14 18:29:21 +0200

    Last 24 hours data daemon creation

--- server/daemons/last_24_hours.js
+++ server/daemons/last_24_hours.js
@@ -1,26 +1,29 @@
-const fs = require('fs');
+var fs = require('fs');
 var moment = require('moment');
 var _ = require('lodash');
 var data_retrieval = require('../utils/data_retrieval.js');
 
-var store_day_data = function(date) {
-    var start = moment(date).startOf('day').format('YYYY-MM-DD[T]HH:mm:ss');
-    var end = moment(date).endOf('day').format('YYYY-MM-DD[T]HH:mm:ss');
-    var date_formatted = moment(date).format('YYYY-MM-DD');
+const INTERVAL = 5*60*1000;
+
+var update = function() {
+    var start = moment().add(-1, 'days').format('YYYY-MM-DD[T]HH:mm:ss');
+    var end = moment().format('YYYY-MM-DD[T]HH:mm:ss');
+    var current = moment().format('YYYY-MM-DD');
     data_retrieval.retrieve(start, end).then(function(data) {
         _.forOwn(data, function(value, key) {
             var path = __dirname + '\\..\\data\\';
-            fs.writeFile(path + date_formatted + '_' + key + '.json', JSON.stringify(value), function(err) {
+            fs.writeFile(path + current + '_' + key + '.json', JSON.stringify(value), function(err) {
                 if(err) {
                     return console.log(err);
                 }
-                console.log(key + " data saved for " + date_formatted);
+                console.log(key + " data saved for " + current);
             });
         });
+        setTimeout(update, INTERVAL);
     }).catch(function(error) {
         console.log('Could not store data');
         console.log(error);
     });
-};
+}
 
-store_day_data(moment().subtract(1, 'day'));
+update();
