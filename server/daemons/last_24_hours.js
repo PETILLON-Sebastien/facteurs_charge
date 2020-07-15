3d6cceeb6c0f5afd805073c7eb4cfae199c2800f
PETILLON-Sebastien <petillon.sebastien@gmail.com>
2020-07-15 21:37:41 +0200

    Last 24 hours deamon with forever

--- server/daemons/last_24_hours.js
+++ server/daemons/last_24_hours.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var moment = require('moment');
 var _ = require('lodash');
+var path = require('path');
 var data_retrieval = require('../utils/data_retrieval.js');
 
 const INTERVAL = 5*60*1000;
@@ -11,12 +12,12 @@ var update = function() {
     var current = moment().format('YYYY-MM-DD');
     data_retrieval.retrieve(start, end).then(function(data) {
         _.forOwn(data, function(value, key) {
-            var path = __dirname + '\\..\\data\\';
-            fs.writeFile(path + current + '_' + key + '.json', JSON.stringify(value), function(err) {
+            var pathToFile = path.resolve(__dirname, '..', 'data', 'current_' + key + '.json');
+            fs.writeFile(pathToFile , JSON.stringify(value), function(err) {
                 if(err) {
                     return console.log(err);
                 }
-                console.log(key + " data saved for " + current);
+                console.log(key + " data saved for " + current + ' at ' + moment().format('YYYY-MM-DD[T]HH:mm:ss'));
             });
         });
         setTimeout(update, INTERVAL);
--- server/daemons/store_previous_day.js
+++ server/daemons/store_previous_day.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 var moment = require('moment');
 var _ = require('lodash');
+var path = require('path');
 var data_retrieval = require('../utils/data_retrieval.js');
 
 var store_day_data = function(date) {
@@ -9,8 +10,8 @@ var store_day_data = function(date) {
     var date_formatted = moment(date).format('YYYY-MM-DD');
     data_retrieval.retrieve(start, end).then(function(data) {
         _.forOwn(data, function(value, key) {
-            var path = __dirname + '\\..\\data\\';
-            fs.writeFile(path + date_formatted + '_' + key + '.json', JSON.stringify(value), function(err) {
+            var pathToFile = path.resolve(__dirname, '..', 'data', current + '_' + key + '.json');
+            fs.writeFile(pathToFile, JSON.stringify(value), function(err) {
                 if(err) {
                     return console.log(err);
                 }
