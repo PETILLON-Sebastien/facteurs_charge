5b5b977b73b03fd2895fc9ddd5229688cef5548c
Benjamin Benni <benjamin.benni06@gmail.com>
2020-07-30 19:42:01 +0200

    Fix missing % sign on load because of server bad response

--- static/js/slides/load-breakdown/components/PowerSourceLoad.jsx
+++ static/js/slides/load-breakdown/components/PowerSourceLoad.jsx
@@ -37,7 +37,7 @@ class PowerSourceLoad extends React.Component {
     buildKPI(kpiName, kpiDescription, cssClass = '') {
         if (kpiDescription != undefined) {
             const kpiValue = Math.round(kpiDescription.value);
-            const kpiUnit = kpiDescription.unit;
+            const kpiUnit = kpiDescription.unit || "%"; //fixme this clause was added to patch the fact that the server does not provide a unit when returning load values
             const kpi = <PowerSourceKPI title={kpiName} value={kpiValue} unit={kpiUnit} cssClass={cssClass} />
             this.state.kpis.push(kpi);
         }
