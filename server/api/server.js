a4dc5faad24f704f0cdda42c7e3bee8f6e0db61d
Benjamin Benni <benjamin.benni06@gmail.com>
2020-08-14 23:47:29 +0200

    Build two environments integration and prod

--- server/api/server.js
+++ server/api/server.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
-var serverPort = 8080;
+var serverPort = process.env.FC_PORT || 8080;
 const cors = require('cors');
 
 // swaggerRouter configuration
