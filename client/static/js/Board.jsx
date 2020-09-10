21f6f1d2279884d399b2fa454d16e72ebbb03c6e
Benjamin Benni <benjamin.benni06@gmail.com>
2020-09-10 09:17:12 +0200

    Split Board file
    Remove server cconnection part from it
    This is WIP definitely

--- client/static/js/Board.jsx
+++ client/static/js/Board.jsx
@@ -1,30 +1,33 @@
-import React, { createRef } from 'react'
+import React, { createRef } from "react";
 
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
+import Server from "./Server";
 
 import SlidePowerSources from "./slides/power-sources-breakdown/components/SlidePowerSources";
-import SlideLoad from './slides/load-breakdown/components/SlideLoad';
-import SlideMap from './slides/map/components/SlideMap';
-import SlidePowerBalance from './slides/power-balance/components/SlidePowerBalance';
-import MyMap from './slides/exchanges/MyMap';
-
+import SlideLoad from "./slides/load-breakdown/components/SlideLoad";
+import SlideMap from "./slides/map/components/SlideMap";
+import SlidePowerBalance from "./slides/power-balance/components/SlidePowerBalance";
+import MyMap from "./slides/exchanges/MyMap";
 
 import stubZonesDescription from "./regions-descriptions";
 
 import _ from "lodash";
 import moment from "moment";
 
-import 'moment/locale/fr';
+import "moment/locale/fr";
 
-
-import { ZoneContext } from './ZoneContext';
-import { element } from 'prop-types';
-const root_endpoint = process.env.API_URL + "/api/v1";
+import { ZoneContext } from "./ZoneContext";
+import { element } from "prop-types";
 
 var that;
 
-export default class Board extends React.Component {
+/**
+ * Avoid the use of async
+ * Data are fetched (this is an async call) but one should not await for it
+ * Each time data are fetched, the "isloaded" global flag is checked, triggering (or not) the shutdown of the loading page
+ */
 
+export default class Board extends React.Component {
   constructor(props) {
     super(props);
     that = this;
@@ -32,15 +35,29 @@ export default class Board extends React.Component {
     this.zonesDescription = this.getZoneDescriptions();
     this.zoneChanged = this.zoneChanged.bind(this);
 
-    const buildNumber ="BUILD_NUMBER_PLACEHOLDER";
-    const buildDate ="BUILD_DATE_PLACEHOLDER";
+    const buildNumber = "BUILD_NUMBER_PLACEHOLDER";
+    const buildDate = "BUILD_DATE_PLACEHOLDER";
     console.log("Build number:", buildNumber);
     this.state = {
       buildNumber: buildNumber,
       buildDate: buildDate,
       currentZone: { id: "xxx", label: "xxxx" }, //fixme,
-      isLoaded: false
+      isLoaded: false,
+      loadingStatuses: [],
     };
+
+    this.state.loadingStatuses.push({
+      name: "fetching power source breakdown",
+      status: "...",
+    });
+    this.state.loadingStatuses.push({
+      name: "fetching load breakdown",
+      status: "...",
+    });
+    this.state.loadingStatuses.push({
+      name: "fetching load for all zones",
+      status: "...",
+    });
 
     console.log("Board built.", "isLoaded?", this.state.isLoaded);
   }
@@ -50,19 +67,23 @@ export default class Board extends React.Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    const shouldBoardUpdate = (nextState.isLoaded) && this.state.currentZone.id != nextState.currentZone.id;
+    const shouldBoardUpdate =
+      nextState.isLoaded &&
+      this.state.currentZone.id != nextState.currentZone.id;
     console.log("Should Board update?", shouldBoardUpdate);
     console.log(this.state, nextState);
     return shouldBoardUpdate;
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     console.log("Board mounted.");
-    await this.zoneChanged(0);
+    this.zoneChanged(0);
   }
-
-  async zoneChanged(newZoneID) {
-    let currentZoneSelected = _.find(this.zonesDescription, { 'id': newZoneID });
+  sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  zoneChanged(newZoneID) {
+    let currentZoneSelected = _.find(this.zonesDescription, { id: newZoneID });
     let labelCurrentZone = currentZoneSelected.label;
 
     let ISOZoneId = "FR";
@@ -70,7 +91,13 @@ export default class Board extends React.Component {
     if (newZoneID != 0) {
       ISOZoneId = ISOZoneId.concat("-").concat(newZoneID);
     }
-    console.log("Zone has changed!", "Old:", this.state.currentZone.id, "New:", ISOZoneId);
+    console.log(
+      "Zone has changed!",
+      "Old:",
+      this.state.currentZone.id,
+      "New:",
+      ISOZoneId
+    );
 
     if (this.state.currentZone.id == ISOZoneId) {
       console.log("Zone hasn't changed.");
@@ -79,197 +106,162 @@ export default class Board extends React.Component {
     }
 
     console.log("Putting the slide power source breakdown in loading mode...");
-    this.setState({ powerSourceBreakdown: { isLoaded: false }, loadBreakdown: { isLoaded: false } });
-
-    let powerSourceData = await this.fetchPowerSourcesBreakdown(ISOZoneId);
-    console.log("Power sources Data fetched.");
-
-    // PRECONDITION: Considering timely ordered data
-    const powerSourcesLatestData = powerSourceData[powerSourceData.length - 1].breakdown;
-
-
-
-    let loadData = await this.fetchLoadsBreakdown(ISOZoneId);
-    console.log("Load data fetched",);
-
-    // PRECONDITION: Considering timely ordered data
-    const latestBreakdownData = loadData[loadData.length - 1].breakdown;
-    console.log("Lastest breakdown", latestBreakdownData);
-
-    let loadDataForAllZones = await this.fetchLoadsForAllZones();
-    console.log("Last load data fetched", loadDataForAllZones);
-
-    // PRECONDITION: Considering timely ordered data
-    // const latestLoads = loadData[loadData.length - 1].breakdown;
-
-    console.log("Modifying state of Board and setting loading state of power source slide to loaded.");
     this.setState({
-      currentZone: { id: ISOZoneId, label: labelCurrentZone },
-      isLoaded: true,
-      powerSourceBreakdown: {
-        isLoaded: true,
-        latestPowerBreakdown: powerSourcesLatestData,
-        powerBreakdownHistory: powerSourceData,
-      },
-      loadBreakdown:
-      {
-        isLoaded: true,
-        latestBreakdownData: latestBreakdownData,
-        breakdownHistory: loadData
-      },
-      highestLoads: loadDataForAllZones
-    });
-  }
-
-
-  async fetchPowerSourcesBreakdown(ISOZoneId) {
-    const targetUrl = root_endpoint + "/zones/" + ISOZoneId + "/installations/production/breakdown";
-    console.log("Fetching power sources", targetUrl);
-
-    let data = await fetch(targetUrl);
-    data = await data.json();
-
-    // Sort data
-    // ISSUE https://github.com/PETILLON-Sebastien/facteurs_charge/issues/52
-    data.sort((breakdownA, breakdownB) => {
-      return moment(breakdownA.datetime).valueOf() - moment(breakdownB.datetime).valueOf();
+      powerSourceBreakdown: { isLoaded: false },
+      loadBreakdown: { isLoaded: false },
     });
 
+    const loadBreakdown = Server.getLoadsBreakdown(ISOZoneId);
+    const powerSourcesBreadown = Server.getPowerSourcesBreakdown(ISOZoneId);
+    const loadsBreakdownForAllZones = Server.getLoadsForAllZones(ISOZoneId);
 
-    // Handling cases where the server does not send proper data
-    let copyOfData = Object.assign(data);
+    Promise.all([
+      loadBreakdown,
+      powerSourcesBreadown,
+      loadsBreakdownForAllZones,
+    ]).then((values) => {
+      console.log(values);
+      let loadBreakdown = values[0];
+      let powerSourceData = values[1];
+      let loadDataForAllZones = values[2];
 
-    data.forEach((currentData, i) => {
-      const breakdown = currentData.breakdown;
+      const powerSourcesLatestData =
+        powerSourceData[powerSourceData.length - 1].breakdown;
 
-      Object.keys(breakdown).forEach((installationType) => {
-        if (breakdown[installationType] === {} || breakdown[installationType].power === undefined) {
-          console.warn("Slide power source, finding highest source of poweer, installation", installationType, "is not defined or has no power field");
-          delete copyOfData[i].breakdown[installationType];
-        }
+      // PRECONDITION: Considering timely ordered data
+      const latestBreakdownData =
+        loadBreakdown[loadBreakdown.length - 1].breakdown;
+      console.log("Lastest breakdown", latestBreakdownData);
+
+      // PRECONDITION: Considering timely ordered data
+      const latestLoads = loadBreakdown[loadBreakdown.length - 1].breakdown;
+
+      console.log(
+        "Modifying state of Board and setting loading state of power source slide to loaded."
+      );
+      this.setState({
+        currentZone: { id: ISOZoneId, label: labelCurrentZone },
+        isLoaded: true,
+        powerSourceBreakdown: {
+          isLoaded: true,
+          latestPowerBreakdown: powerSourcesLatestData,
+          powerBreakdownHistory: powerSourceData,
+        },
+        loadBreakdown: {
+          isLoaded: true,
+          latestBreakdownData: latestBreakdownData,
+          breakdownHistory: loadBreakdown,
+        }, // this can be built in the callback of the async.parallel
+        highestLoads: loadDataForAllZones,
       });
-    })
-
-    data = copyOfData;
-
-
-
-    let now = moment().valueOf();
-    data = data.filter((element) => {
-      return moment(element.datetime).valueOf() < now;
     });
-    // Remove the last one (likely 0)
-    // https://github.com/PETILLON-Sebastien/facteurs_charge/issues/53
-    // data.pop();
-    // data.pop();
-    // data.pop();
-    // ------------------------------------------------------------------------
-
-    return data;
   }
-
-  async fetchLoadsBreakdown(ISOZoneId) {
-    const targetUrl = root_endpoint + "/zones/" + ISOZoneId + "/installations/breakdown";
-    console.log("Fetching loads", targetUrl);
-
-    let data = await fetch(targetUrl);
-
-    data = await data.json();
-
-    let i = 0, j = 0;
-
-    for (i = 0; i < data.length; i++) {
-      let currentBreakdown = data[i].breakdown;
-      let keys = Object.keys(currentBreakdown);
-      for (j = 0; j < keys.length; j++) {
-        const currentKey = keys[j];
-
-        // PATCH https://github.com/PETILLON-Sebastien/facteurs_charge/issues/58
-        if (currentKey == "nuclear") {
-          delete data[i].breakdown["nuclear"];
-          continue;
-        }
-        const currentLoad = currentBreakdown[currentKey].load.value;
-        const updatedLoad = currentLoad * 100;
-        currentBreakdown[currentKey].load.value = updatedLoad;
-
-      }
-    }
-    return data;
-  }
-
-  async fetchLoadsForAllZones() {
-    const targetUrl = root_endpoint + "/zones/installations/load/last?filter=highest";
-    console.log("Fetching last loads", targetUrl);
-
-    let data = await fetch(targetUrl);
-
-    data = await data.json();
-    let i = 0, j = 0;
-
-    let result = {};
-
-    for (i = 0; i < data.length; i++) {
-      let currentData = data[i];
-      result[currentData.zoneId] = currentData.snapshots[0].highest;
-    }
-    return result;
-  }
-
 
   render() {
     console.log("Rendering Board...");
 
     if (!this.state.isLoaded) {
-      console.log("Board has not been fully loaded yet, aborting the rendering and displaying loading screen instead.");
-      return <div className={`pageloader is-dark ${this.state.done ? "" : "is-active"}`} ref="spinner">
-        <span className="title">Facteurs charge préchauffe... On arrive !</span>
-      </div>
+      console.log(
+        "Board has not been fully loaded yet, aborting the rendering and displaying loading screen instead."
+      );
+      return (
+        <div
+          className={`pageloader is-dark ${this.state.done ? "" : "is-active"}`}
+          ref="spinner"
+        >
+          <span className="title">
+            Facteurs charge préchauffe... On arrive !
+          </span>
+          <div>
+            <ul>
+              {this.state.loadingStatuses.map((item, i) => {
+                return (
+                  <li key={i}>
+                    <span>{item.name}</span>
+                    <span>{item.status}</span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      );
     }
 
     let powerSourceSlide = null;
     if (this.state.powerSourceBreakdown.isLoaded) {
-      powerSourceSlide = <SlidePowerSources currentZone={this.state.currentZone} data={this.state.powerSourceBreakdown} />
+      powerSourceSlide = (
+        <SlidePowerSources
+          currentZone={this.state.currentZone}
+          data={this.state.powerSourceBreakdown}
+        />
+      );
     } else {
-      powerSourceSlide =
-        <div className="section is-medium" style={{ "minHeight": "100vh" }}>
+      powerSourceSlide = (
+        <div className="section is-medium" style={{ minHeight: "100vh" }}>
           <div className="container">
-            <div className="columns is-vcentered has-text-centered" style={{ "marginTop": "30vh" }}>
+            <div
+              className="columns is-vcentered has-text-centered"
+              style={{ marginTop: "30vh" }}
+            >
               <div className="column is-full">
                 <div className="lds-dual-ring"></div>
               </div>
             </div>
           </div>
         </div>
+      );
     }
 
     let loadBreakdownSlide = null;
     if (this.state.loadBreakdown.isLoaded) {
-      loadBreakdownSlide = <SlideLoad currentZone={this.state.currentZone} data={this.state.loadBreakdown} />
-
+      loadBreakdownSlide = (
+        <SlideLoad
+          currentZone={this.state.currentZone}
+          data={this.state.loadBreakdown}
+        />
+      );
     } else {
-      loadBreakdownSlide =
-        <div className="section is-medium" style={{ "minHeight": "100vh" }}>
+      loadBreakdownSlide = (
+        <div className="section is-medium" style={{ minHeight: "100vh" }}>
           <div className="container">
-            <div className="columns is-vcentered has-text-centered" style={{ "marginTop": "30vh" }}>
+            <div
+              className="columns is-vcentered has-text-centered"
+              style={{ marginTop: "30vh" }}
+            >
               <div className="column is-full">
                 <div className="lds-dual-ring"></div>
               </div>
             </div>
           </div>
         </div>
+      );
     }
-
 
     return (
       <React.Fragment>
-        <header >
-          <Navbar that={that} label_region={this.state.currentZone.label} hookZoneChanged={this.zoneChanged} zonesDescription={this.zonesDescription} />
+        <header>
+          <Navbar
+            that={that}
+            label_region={this.state.currentZone.label}
+            hookZoneChanged={this.zoneChanged}
+            zonesDescription={this.zonesDescription}
+          />
         </header>
 
-        <div className="section is-small" id="slide-map" style={{ "marginTop": "0rem" }}>
+        <div
+          className="section is-small"
+          id="slide-map"
+          style={{ marginTop: "0rem" }}
+        >
           <div className="container">
-            <SlideMap zoneChanged={this.zoneChanged} zonesDescription={this.zonesDescription} highestLoads={this.state.highestLoads} buildNumber={this.state.buildNumber} buildDate={this.state.buildDate}/>
+            <SlideMap
+              zoneChanged={this.zoneChanged}
+              zonesDescription={this.zonesDescription}
+              highestLoads={this.state.highestLoads}
+              buildNumber={this.state.buildNumber}
+              buildDate={this.state.buildDate}
+            />
           </div>
         </div>
 
@@ -292,6 +284,6 @@ export default class Board extends React.Component {
           </div> */}
         {/* </ZoneContext.Provider> */}
       </React.Fragment>
-    )
+    );
   }
 }
---
+++ client/static/js/Server.js
@@ -0,0 +1,154 @@
+var async = require("async");
+
+const root_endpoint = process.env.API_URL + "/api/v1";
+
+export default class Server {
+    static getPowerSourcesBreakdown(ISOZoneId, callback) {
+        const targetUrl =
+            root_endpoint +
+            "/zones/" +
+            ISOZoneId +
+            "/installations/production/breakdown";
+        console.log("Fetching power sources", targetUrl);
+        async.waterfall(
+            [
+                (cb) => {
+                    fetch(targetUrl).then((rawData) => cb(null, rawData));
+                },
+                (rawData, cb) => {
+                    rawData.json().then((data) => cb(data));
+                },
+            ],
+            (err, data) => {
+                // Sort data
+                // ISSUE https://github.com/PETILLON-Sebastien/facteurs_charge/issues/52
+                data.sort((breakdownA, breakdownB) => {
+                    return (
+                        moment(breakdownA.datetime).valueOf() -
+                        moment(breakdownB.datetime).valueOf()
+                    );
+                });
+
+                // Handling cases where the server does not send proper data
+                let copyOfData = Object.assign(data);
+
+                data.forEach((currentData, i) => {
+                    const breakdown = currentData.breakdown;
+
+                    Object.keys(breakdown).forEach((installationType) => {
+                        if (
+                            breakdown[installationType] === {} ||
+                            breakdown[installationType].power === undefined
+                        ) {
+                            console.warn(
+                                "Slide power source, finding highest source of poweer, installation",
+                                installationType,
+                                "is not defined or has no power field"
+                            );
+                            delete copyOfData[i].breakdown[installationType];
+                        }
+                    });
+                });
+
+                data = copyOfData;
+
+                let now = moment().valueOf();
+                data = data.filter((element) => {
+                    return moment(element.datetime).valueOf() < now;
+                });
+                // Remove the last one (likely 0)
+                // https://github.com/PETILLON-Sebastien/facteurs_charge/issues/53
+                // data.pop();
+                // data.pop();
+                // data.pop();
+                // ------------------------------------------------------------------------
+
+                const elementsIndex = this.state.loadingStatuses.findIndex(
+                    (elem) => elem.name == "fetching power source breakdown"
+                );
+                const newArray = [...this.state.loadingStatuses];
+
+                newArray[elementsIndex].status = "OK!";
+                console.log(newArray);
+                this.setState({
+                    loadingStatuses: newArray,
+                });
+
+                callback(data);
+            }
+        );
+    }
+
+    static getLoadsBreakdown(ISOZoneId, callback) {
+        const targetUrl =
+            root_endpoint + "/zones/" + ISOZoneId + "/installations/breakdown";
+        console.log("Fetching loads", targetUrl);
+
+        async.waterfall(
+            [
+                (cb) => {
+                    fetch(targetUrl).then((rawData) => cb(null, rawData));
+                },
+                (rawData, cb) => {
+                    rawData.json().then((data) => cb(data));
+                },
+            ],
+            (err, data) => {
+                let i = 0,
+                    j = 0;
+
+                for (i = 0; i < data.length; i++) {
+                    let currentBreakdown = data[i].breakdown;
+                    let keys = Object.keys(currentBreakdown);
+                    for (j = 0; j < keys.length; j++) {
+                        const currentKey = keys[j];
+
+                        // PATCH https://github.com/PETILLON-Sebastien/facteurs_charge/issues/58
+                        if (currentKey == "nuclear") {
+                            delete data[i].breakdown["nuclear"];
+                            continue;
+                        }
+                        const currentLoad = currentBreakdown[currentKey].load.value;
+                        const updatedLoad = currentLoad * 100;
+                        currentBreakdown[currentKey].load.value = updatedLoad;
+                    }
+                }
+
+                this.state.loadingStatuses.push({
+                    name: "fetching load breakdown",
+                    status: "OK!",
+                });
+
+                callback(data);
+            }
+        );
+    }
+
+    static getLoadsForAllZones(callback) {
+        const targetUrl =
+            root_endpoint + "/zones/installations/load/last?filter=highest";
+        console.log("Fetching last loads", targetUrl);
+
+        async.waterfall(
+            [
+                (cb) => {
+                    fetch(targetUrl).then((rawData) => cb(null, rawData));
+                },
+                (rawData, cb) => {
+                    rawData.json().then((data) => cb(data));
+                },
+            ],
+            (err, data) => {
+                let i = 0;
+
+                let result = {};
+
+                for (i = 0; i < data.length; i++) {
+                    let currentData = data[i];
+                    result[currentData.zoneId] = currentData.snapshots[0].highest;
+                }
+                callback(data);
+            }
+        );
+    }
+}
