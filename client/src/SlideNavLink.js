4a91c61acf7991c33fd90ff0e5a4158d21e49e79
Benjamin Benni <benjamin.benni06@gmail.com>
2020-11-06 22:49:51 +0100

    Fix no return in SlideNavLink and activeTab props missing

--- client/src/SlideNavLink.js
+++ client/src/SlideNavLink.js
@@ -2,21 +2,23 @@
 
 import React from "react";
 
-function SlideNavLink({ slideName, setCurrentSlide, slideAnchor }) {
-    <span className="navbar-item">
-        <a href="#slide-map">
-            <button
-                className={`${this.state.activeTab == slideAnchor ? "is-success" : ""
-                    } is-primary button is-fullwidth`}
-                onClick={() => setCurrentSlide(slideAnchor)}
-            >
-                <span className="icon">
-                    <i className="fas fa-map-marker-alt"></i>
-                </span>
-                <span>{slideName}</span>
-            </button>
-        </a>
-    </span>
+function SlideNavLink({ slideName, setCurrentSlide, slideAnchor, activeTab }) {
+    return (
+        <span className="navbar-item">
+            <a href="#slide-map">
+                <button
+                    className={`${activeTab == slideAnchor ? "is-success" : ""
+                        } is-primary button is-fullwidth`}
+                    onClick={() => setCurrentSlide(slideAnchor)}
+                >
+                    <span className="icon">
+                        <i className="fas fa-map-marker-alt"></i>
+                    </span>
+                    <span>{slideName}</span>
+                </button>
+            </a>
+        </span>
+    )
 }
 
 export default SlideNavLink;
