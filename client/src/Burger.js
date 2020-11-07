61c027cc5ce61182067d7a2c5e7ae6a8342ade9a
Benjamin Benni <benjamin.benni06@gmail.com>
2020-11-07 09:28:42 +0100

    Update Burger.js

--- client/src/Burger.js
+++ client/src/Burger.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Twitter({ showMobileMenu, setMenuToggle }) {
+function Twitter({ isMobileMenuShown, setShowMenu }) {
     return (
         <a
             role="button"
-            className={`navbar-burger burger ${showMobileMenu ? "is-active" : ""
+            className={`navbar-burger burger ${isMobileMenuShown ? "is-active" : ""
                 }`}
-            onClick={setMenuToggle}
+            onClick={setShowMenu}
             aria-label="menu"
             aria-expanded="false"
             data-target="navbarBasicExample"
