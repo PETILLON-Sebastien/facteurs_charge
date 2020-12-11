2f0813df5b2554c59823930041ba6f0f5628e25b
Benjamin Benni <benjamin.benni06@gmail.com>
2020-12-11 09:46:05 +0100

    Fix bug in Calendar value prop format

--- client/src/CurrentDateSelection.js
+++ client/src/CurrentDateSelection.js
@@ -3,6 +3,12 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 function CurrentDateSelection({ currentDatesFrom, currentDatesTo, setShowDateSelection, setCurrentDate }) {
+    /* MIN DATE HAS BEEN HARDCODED PER @Petillon's requirements due to backend limitation:
+        https://github.com/PETILLON-Sebastien/facteurs_charge/issues/65#issue-688691869  */
+    const minDate = new Date("2020-02-01T00:00:00.00Z");
+    const now = new Date();
+    const convertedCurrenDateFrom = new Date(currentDatesFrom);
+    const convertedCurrenDateTo = new Date(currentDatesTo);
 
     return (
         <div
@@ -14,19 +20,18 @@ function CurrentDateSelection({ currentDatesFrom, currentDatesTo, setShowDateSel
                 onClick={() => setShowDateSelection(false)}
             ></div>
             <div className="modal-content">
-                {/* MIN DATE HAS BEEN HARDCODED PER @Petillon's requirements due to backend limitation: https://github.com/PETILLON-Sebastien/facteurs_charge/issues/65#issue-688691869  */}
                 <Calendar
                     returnDate="range"
                     selectRange={true}
-                    maxDate={new Date()}
-                    minDate={new Date("2020-02-01T00:00:00.00Z")}
+                    maxDate={now}
+                    minDate={minDate}
                     onChange={(values) => {
                         setShowDateSelection(false);
                         const newDateFrom = new Date(values[0]).getTime();
                         const newDateTo = new Date(values[1]).getTime();
                         setCurrentDate({ from: newDateFrom, to: newDateTo });
                     }}
-                    value={[currentDatesFrom, currentDatesTo]}
+                    value={[convertedCurrenDateFrom, convertedCurrenDateTo]}
                 />
             </div>
             <button
