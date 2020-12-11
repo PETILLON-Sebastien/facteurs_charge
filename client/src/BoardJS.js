aabda2d4764a2875d3ec6a93c9599e74cd97f7f6
Benjamin Benni <benjamin.benni06@gmail.com>
2020-12-11 22:46:09 +0100

    Fix Board margin

--- client/src/BoardJS.js
+++ client/src/BoardJS.js
@@ -1,21 +1,24 @@
 import React from "react";
 import SlideMap from "./SlideMap";
+import SlidePowerSources from "./SlidePowerSources";
 
-function BoardJS({ setCurrentZone, slideMapData, buildInfos = { number: -1, date: new Date().getTime() } }) {
+function BoardJS({
+    currentZone,
+    setCurrentZone,
+    slideMapData,
+    slidePowerSourcesData,
+    buildInfos = { number: -1, date: new Date().getTime() }
+}) {
     return (
         <React.Fragment>
-            <div
-                className="section is-small"
-                id="slide-map"
-                style={{ marginTop: "0rem" }}
-            >
-                <div className="container">
-
+            <div className="section is-small" id="slide-map" >
+                <div className="container" style={{ width: "90%" }}>
                     <SlideMap
                         setCurrentZone={setCurrentZone} // setter pass throught
                         highestLoads={slideMapData} // props
                         build={buildInfos}
                     />
+                    <SlidePowerSources powerBreakdownHistory={slidePowerSourcesData} currentZone={currentZone} />
                 </div>
             </div>
         </React.Fragment>
