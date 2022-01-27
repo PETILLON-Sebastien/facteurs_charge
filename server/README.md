8721c67a34fdf125f5824407df84ef1f1c38c710
Benjamin Benni <ttben@users.noreply.github.com>
2022-01-27 21:18:32 +0100

    Move Server files into dedicated folder

---
+++ server/README.md
@@ -0,0 +1,54 @@
+# Facteurs de charge
+
+Ceci est le projet de démonstration des facteurs de charge du parc électrique français.
+
+## Environnement
+
+Quelques instructions pour mettre en place l'environnement de travail
+
+### Front-end
+
+Lancez la commande suivante dans le dossier à la racine :
+
+```npm i webpack --save-dev```
+
+Installez les dépendances :
+
+```npm install```
+
+Et mettez en place babel :
+
+```npm i @babel/preset-env @babel/preset-react --save-dev```
+
+```npm install babel-loader```
+
+```npm install --save-dev @babel/core @babel/preset-env```
+
+Pour exécuter le front-end, lancez la commande
+```npm run watch```
+puis ouvrez le fichier `index.html`.
+
+### Back-end
+
+Installez les dépendances :
+
+```npm install```
+
+Pour exécuter le back-end, lancez la commande
+```npm run start```
+
+### Précisions
+
+Correspondances code_insee_region / nom région :
+11 : Ile-de-France
+24 : Centre-Val de Loire
+27 : Bourgogne-Franche-Comté
+28 : Normandie
+32 : Hauts-de-France
+44 : Grand-Est
+52 : Pays de la Loire
+53 : Bretagne
+75 : Nouvelle-Aquitaine
+76 : Occitanie
+84 : Auvergne-Rhône-Alpes
+93 : Provence-Alpes-Côte d'Azur
---
+++ server/api/server.js
@@ -0,0 +1,61 @@
+var http = require('http');
+var fs = require('fs');
+var _ = require('lodash');
+var moment = require('moment');
+var compute = require('./compute.js');
+var express = require('express')
+var app = express()
+
+var dernier_appel = undefined;
+var donnees = undefined;
+var nombre_region = 12;
+var nombre_donnees_par_heure = 4;
+var nombre_heures = 25;
+var periode_rafraichissement = 5;
+
+function serveData(res) {
+    res.writeHead(200, {"Content-Type": "application/json"});
+    if(compute.appel_necessaire(dernier_appel, periode_rafraichissement)) {
+        compute.recuperation_donnes_api(nombre_region, nombre_donnees_par_heure, nombre_heures, function (api_response) {
+            try {
+                var donnees_calculees = compute.construire_donnees(api_response, nombre_donnees_par_heure, nombre_heures, true);
+                donnees = donnees_calculees;
+                dernier_appel = moment();
+            } catch (e) {
+                console.error(e);
+            }
+            res.end(JSON.stringify(donnees));
+        });
+    } else {
+        res.end(JSON.stringify(donnees));
+    }
+}
+
+function serveFrontEnd(res) {
+    fs.readFile('./static/facteurscharge.html', function (err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        } else {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+            return;
+        }
+    });
+}
+
+app.use(express.static("static"));
+app.use(express.static("donnees"));
+
+app.get('/', function (req, res) {
+    serveFrontEnd(res);
+});
+app.get('/region/[0-9]+', function (req, res) {
+    serveFrontEnd(res);
+});
+app.get('/now', (req, res) => {
+    serveData(res);
+});
+
+app.listen(8080, () => console.log(`Application lancée`));
